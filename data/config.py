3860bd29ccfd2fa38e1811655a0c9208bc9b8a6a
xuzhang <xzhcn6@outlook.com>
2019-12-09 23:11:32 +0800

    Updata default batch_size and max_iter

--- data/config.py
+++ data/config.py
@@ -46,7 +46,7 @@ coco = {
 sixray = {
     'num_classes': 3,
     'lr_steps': (80000, 100000, 120000),
-    'max_iter': 300,
+    'max_iter': 200000,
     'feature_maps': [38, 19, 10, 5, 3, 1],
     'min_dim': 300,
     'steps': [8, 16, 32, 64, 100, 300],
